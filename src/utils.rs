7a9b1f1132525b4888dd1fad10eea8cbbc7e2512
Márcio <marcio@archdesk.localdomain>
2023-02-20 11:59:16 -0300

    Include the utils.rs file

---
+++ src/utils.rs
@@ -0,0 +1,7 @@
+macro_rules! get_widget {
+	($builder:expr, $wtype:ty, $name:ident) => {
+		let $name: $wtype = $builder.object(stringify!($name)).expect(&format!("Could not find widget \"{}\"", stringify!($name)));
+	};
+}
+
+
